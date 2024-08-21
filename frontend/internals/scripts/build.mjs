79c01e01ad0dc790d2ca89344d105ba624abea48
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-08-21 16:25:05 +0200

    Build staging
    - add base url for GitHub pages

--- frontend/internals/scripts/build.mjs
+++ frontend/internals/scripts/build.mjs
@@ -7,6 +7,9 @@ import { execSync } from 'child_process'
 const buildSha = execSync('git rev-parse HEAD').toString().trim()
 const buildDatetime = Date.now().toString()
 
-execSync(`REACT_APP_BUILD_SHA="${buildSha}" REACT_APP_BUILD_DATETIME="${buildDatetime}" vite build`, {
-  stdio: 'inherit',
-})
+execSync(
+  `REACT_APP_BUILD_SHA="${buildSha}" REACT_APP_BUILD_DATETIME="${buildDatetime}" vite build --mode staging --base /dapp-staker/`,
+  {
+    stdio: 'inherit',
+  }
+)
