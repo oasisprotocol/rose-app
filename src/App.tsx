848c3ca9c8d81000cd09c617600b66997ec69a4a
lukaw3d <lukajeran@oasislabs.com>
2024-08-24 02:58:52 +0200

    Extract useGenerateConsensusAccount

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { useEffect, useState } from 'react'
-import { bytesToHex, hexToBytes } from 'viem'
-import { useAccount, useAccountEffect, useSignMessage } from 'wagmi'
-import { siweMessageConsensusToSapphire } from './utils/siweMessageConsensusToSapphire'
-
-function toBase64(u8: Uint8Array) {
-  return btoa(String.fromCharCode(...u8))
-}
+import { useAccount, useAccountEffect } from 'wagmi'
+import { useGenerateConsensusAccount } from './utils/useGenerateConsensusAccount'
 
 function App() {
   const latestConnectedAccount = useAccount()
   const [firstConnectedAccount, setFirstConnectedAccount] = useState<`0x${string}`>()
-  const { signMessageAsync } = useSignMessage()
-  const [stakingSecret, setConsensusSecret] = useState<Uint8Array>()
+  const { consensusAccount, generateConsensusAccount } = useGenerateConsensusAccount()
 
   useAccountEffect({
     onConnect: (d) => {
@@ -27,40 +21,9 @@ function App() {
     }
   }, [firstConnectedAccount, latestConnectedAccount.address])
 
-  // When 'Generate' button is pressed, do SIWE then derive Consensus key
-  async function generateKeypair() {
-    console.log('generateKeypair for', latestConnectedAccount.address)
-    if (latestConnectedAccount.address) {
-      const signature = await signMessageAsync({
-        message: siweMessageConsensusToSapphire(latestConnectedAccount.address),
-      })
-      console.log('Signature is', signature)
-      const digest = await window.crypto.subtle.digest('SHA-512', hexToBytes(signature))
-      console.log('Digest', digest)
-      const secret = new Uint8Array(digest)
-      if (!stakingSecret || bytesToHex(secret) !== bytesToHex(stakingSecret)) {
-        setConsensusSecret(secret)
-      }
-    }
-  }
-
-  function CopyStakingSecret() {
-    if (stakingSecret) {
-      navigator.clipboard.writeText(toBase64(stakingSecret))
-    }
-  }
-
-  function GenerateButton() {
-    if (stakingSecret) {
-      return (
-        <div>
-          Secret: <input type="text" readOnly value={toBase64(stakingSecret)}></input>
-          <button onClick={CopyStakingSecret}>&#x2398;</button>
-          <br />
-        </div>
-      )
-    } else {
-      return <button onClick={generateKeypair}>Generate</button>
+  async function step2() {
+    if (firstConnectedAccount) {
+      await generateConsensusAccount(firstConnectedAccount)
     }
   }
 
@@ -74,14 +37,19 @@ function App() {
 
       <div>
         <h2>Step 2</h2>
-        <div>
-          <button>Switch Chain to Sapphire</button>
-        </div>
-      </div>
+        <button type="button" onClick={step2}>
+          Select and sign-in
+        </button>
 
-      <div>
-        <h2>Step 3</h2>
-        <GenerateButton />
+        {consensusAccount && (
+          <div>
+            Secret: <input type="text" readOnly value={consensusAccount.privateKey}></input>
+            <button type="button" onClick={() => window.navigator.clipboard.writeText(consensusAccount.privateKey)}>
+              &#x2398;
+            </button>
+            <br />
+          </div>
+        )}
       </div>
     </>
   )
---
+++ src/utils/useGenerateConsensusAccount.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { hexToBytes } from 'viem'
+import { useSignMessage } from 'wagmi'
+import { siweMessageConsensusToSapphire } from './siweMessageConsensusToSapphire'
+
+function toBase64(u8: Uint8Array) {
+  return btoa(String.fromCharCode(...u8))
+}
+
+export function useGenerateConsensusAccount() {
+  const { signMessageAsync } = useSignMessage()
+  const [consensusAccount, setConsensusAccount] = useState<{ privateKey: string }>()
+
+  return {
+    consensusAccount,
+    generateConsensusAccount: async (address: `0x${string}`) => {
+      const signature = await signMessageAsync({
+        message: siweMessageConsensusToSapphire(address),
+      })
+      const digest = await window.crypto.subtle.digest('SHA-512', hexToBytes(signature))
+      const privateKey = toBase64(new Uint8Array(digest))
+      const account = { privateKey }
+      setConsensusAccount(account)
+      return account
+      // Ignore errors
+    },
+  }
+}
