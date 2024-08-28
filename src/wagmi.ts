4b41196be834565d1bf081e393bc7c36c9b7d22c
lukaw3d <lukajeran@oasislabs.com>
2024-08-28 05:10:55 +0200

    Configure WalletConnect project ID

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,7 +3,7 @@ import { sapphire } from 'wagmi/chains'
 
 export const config = getDefaultConfig({
   appName: 'ROSE On-Ramp',
-  projectId: 'YOUR_PROJECT_ID', //
+  projectId: import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID,
   chains: [sapphire],
   ssr: false, // If your dApp uses server side rendering (SSR)
   batch: {
