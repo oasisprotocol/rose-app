966e08b284b98957d2ad30b127620c01d04170a5
lukaw3d <lukajeran@oasislabs.com>
2024-08-28 06:02:15 +0200

    Split UI stages and add more text

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ function App() {
         consensusAddress: consensusAccount.address,
         sapphireAddress: sapphireAddress,
       })
-      setProgress({ percentage: 0.5, message: `${amountToDeposit.formatted} ROSE detected (set allowance)` })
+      setProgress({ percentage: 0.5, message: `${amountToDeposit.formatted} ROSE detected (updated allowance)` })
       const preDepositSapphireBalance = await getSapphireBalance(sapphireAddress)
       await depositToSapphireStep2({
         amountToDeposit: amountToDeposit.raw,
@@ -56,11 +56,11 @@ function App() {
         consensusAddress: consensusAccount.address,
         sapphireAddress: sapphireAddress,
       })
-      setProgress({ percentage: 0.75, message: `${amountToDeposit.formatted} ROSE detected (submit deposit)` })
+      setProgress({ percentage: 0.75, message: `${amountToDeposit.formatted} ROSE detected (sent deposit)` })
       await waitForSapphireBalance(sapphireAddress, preDepositSapphireBalance.raw)
       setProgress({
         percentage: 1.0,
-        message: `${amountToDeposit.formatted} deposited (balance changed, event didn't error)`,
+        message: `${amountToDeposit.formatted} deposited (balance changed)`,
       })
       allowNavigatingAway()
       await updateBalanceInsideConnectButton()
@@ -78,39 +78,80 @@ function App() {
     await step3(consensusAccount, sapphireAddress)
   }
 
-  return (
-    <>
+  if (!sapphireAddress) {
+    return (
       <div>
-        <h1>Step 1</h1>
+        <p style={{ maxWidth: '670px' }}>
+          The Rose (on)ramp dApp has been built by Oasis to offer users an easy to use service of moving your Rose from
+          any centralized exchange to Sapphire (Metamask), without having to handle a Rose Wallet. Connect below and
+          follow the steps to complete your transfer.
+        </p>
+        {/* step1 */}
         <ConnectButton />
       </div>
-
+    )
+  }
+  if (!consensusAccount) {
+    return (
       <div>
-        <h2>Step 2</h2>
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <ConnectButton />
+        </div>
+
+        <p>Consensus to Sapphire</p>
         <button type="button" onClick={step2}>
           Select and sign-in
         </button>
 
-        {consensusAccount && (
-          <div>
-            <AccountAvatar diameter={24} account={{ address: consensusAccount.address }} />
-            {consensusAccount.address}
-            <br />
-            Private key: <input type="text" readOnly value={consensusAccount.privateKey}></input>
-            <button type="button" onClick={() => window.navigator.clipboard.writeText(consensusAccount.privateKey)}>
-              &#x2398;
-            </button>
-            <br />
-          </div>
-        )}
+        <p>Sapphire to Consensus</p>
+        <button type="button" disabled>
+          Coming soon
+        </button>
       </div>
+    )
+  }
+  return (
+    <div>
       <div>
-        <h2>Progress</h2>
-        {progress.percentage}
+        <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <ConnectButton />
+        </div>
+
+        <h2>Address created and awaiting your transfer</h2>
+        <p style={{ maxWidth: '670px' }}>
+          Use the generated address below to initiate the transfer on your centralized exchange. The dApp automatically
+          recognizes your transfer form the centralized exchange and will track it.
+        </p>
+
+        <div>
+          Your address &nbsp;
+          <AccountAvatar diameter={24} account={{ address: consensusAccount.address }} />
+          {consensusAccount.address}
+          &nbsp;
+          <button type="button" onClick={() => window.navigator.clipboard.writeText(consensusAccount.address)}>
+            Copy address &#x2398;
+          </button>
+          &nbsp;
+          <button type="button" onClick={() => window.navigator.clipboard.writeText(consensusAccount.privateKey)}>
+            Copy private key &#x2398;
+          </button>
+        </div>
+        <br />
         <br />
         {progress.message}
+        <br />
+        <progress id="file" value={progress.percentage} max={1} style={{ width: '30vw', height: '32px' }} />
+        <br />
+        <br />
+        <br />
+        <br />
+
+        <p style={{ maxWidth: '670px' }}>
+          Please do not close this window in order to complete the process. If the window is closed you can always
+          recover from the last step and your funds won't be lost.
+        </p>
       </div>
-    </>
+    </div>
   )
 }
 
