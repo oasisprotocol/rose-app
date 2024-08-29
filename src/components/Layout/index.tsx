3cc3083e6ac4955091148e07e9829a767c7db9d5
lukaw3d <lukajeran@oasislabs.com>
2024-08-29 05:23:03 +0200

    Migrate layout to this codebase

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,46 +1,10 @@
-import { FC } from 'react'
-import { NavLink, Outlet, useMatch } from 'react-router-dom'
-import classes from './index.module.css'
-import { LogoIcon } from '../icons/LogoIcon'
-import { ConnectWallet } from '../ConnectWallet'
-import { Alert } from '../Alert'
-import { useAppState } from '../../hooks/useAppState'
-import { Button } from '../Button'
-import { StringUtils } from '../../utils/string.utils'
 import { LayoutBase } from '../LayoutBase'
-import { OasisIcon } from '../icons/OasisIcon'
+import classes from './index.module.css'
 
-export const Layout: FC = () => {
-  const isHomePage = useMatch('/')
-  const {
-    state: { appError },
-    clearAppError,
-  } = useAppState()
-
+export function Layout(props: { header: React.ReactNode; children: React.ReactNode }) {
   return (
-    <LayoutBase
-      header={
-        <header className={classes.header}>
-          <NavLink to="/dashboard">{isHomePage ? <OasisIcon /> : <LogoIcon />}</NavLink>
-          {!isHomePage && <ConnectWallet />}
-        </header>
-      }
-    >
-      <section className={classes.mainSection}>
-        {appError && (
-          <Alert
-            type="error"
-            actions={
-              <Button variant="text" onClick={clearAppError}>
-                &lt; Go back&nbsp;
-              </Button>
-            }
-          >
-            {StringUtils.truncate(appError)}
-          </Alert>
-        )}
-        {!appError && <Outlet />}
-      </section>
+    <LayoutBase header={<header className={classes.header}>{props.header}</header>}>
+      <section className={classes.mainSection}>{props.children}</section>
     </LayoutBase>
   )
 }
--- src/components/LayoutBase/index.tsx
+++ src/components/LayoutBase/index.tsx
@@ -1,23 +1,11 @@
 import { FC, PropsWithChildren, ReactNode } from 'react'
 import classes from './index.module.css'
-import {
-  GITHUB_REPOSITORY_URL,
-  OASIS_DOCS_PAGE_URL,
-  OASIS_HOME_PAGE_URL,
-  VITE_APP_VERSION,
-  VITE_REACT_APP_BUILD_DATETIME,
-  VITE_REACT_APP_BUILD_VERSION,
-} from '../../constants/config'
-import { DateUtils } from '../../utils/date.utils'
-import { useMediaQuery } from 'react-responsive'
 
 interface Props {
   header?: ReactNode
 }
 
 export const LayoutBase: FC<PropsWithChildren<Props>> = ({ children, header }) => {
-  const isDesktopScreen = useMediaQuery({ query: '(min-width: 1000px)' })
-
   return (
     <div className={classes.layout}>
       {header}
@@ -25,29 +13,15 @@ export const LayoutBase: FC<PropsWithChildren<Props>> = ({ children, header }) =
       <footer className={classes.footer}>
         <div className={classes.footerColumn}>
           <span>
-            {VITE_REACT_APP_BUILD_VERSION && VITE_REACT_APP_BUILD_DATETIME && (
-              <div>
-                Version: {VITE_APP_VERSION} (commit:{' '}
-                <a
-                  href={`${GITHUB_REPOSITORY_URL}commit/${VITE_REACT_APP_BUILD_VERSION}`}
-                  rel="noopener noreferrer"
-                  target="_blank"
-                >
-                  {VITE_REACT_APP_BUILD_VERSION.substring(0, 7)}
-                </a>
-                ) built at{' '}
-                {DateUtils.intlDateFormat(VITE_REACT_APP_BUILD_DATETIME, {
-                  format: isDesktopScreen ? 'long' : 'short',
-                })}
-              </div>
-            )}
+            <div>
+              Version: {APP_VERSION} (commit:{' '}
+              <a href={`${GITHUB_REPOSITORY_URL}commit/${BUILD_COMMIT}`} rel="noopener noreferrer" target="_blank">
+                {BUILD_COMMIT.substring(0, 7)}
+              </a>
+              ) built at {BUILD_DATETIME}
+            </div>
           </span>
           <span>|</span>
-          <span>
-            <a href={OASIS_DOCS_PAGE_URL} rel="noopener noreferrer" target="_blank">
-              API Documentation
-            </a>
-          </span>
           <span>
             <a href={GITHUB_REPOSITORY_URL} rel="noopener noreferrer" target="_blank">
               GitHub
