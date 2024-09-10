020e63ebfa2be3b7fae8bd52a4a03c0c299a5779
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-09-10 21:10:02 +0200

    Remove paratime runtime id from frontend

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   VITE_NETWORK: string
-  VITE_PARATIME_RUNTIME_ID: string
   VITE_REACT_APP_BUILD_VERSION: string
   VITE_REACT_APP_BUILD_DATETIME: string
   VITE_NEXUS_BASE_URL: string
