f1f417278609db4f76ce2948ccae643d5f621655
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-09-12 11:05:42 +0200

    Remove singletons in subcall lib

--- subcall/src/query.ts
+++ subcall/src/query.ts
@@ -3,70 +3,30 @@ import * as oasis from '@oasisprotocol/client'
 import * as oasisRT from '@oasisprotocol/client-rt'
 import { Delegations, Undelegations } from './types.ts'
 
+type DelegationsQueryWrapperArgs = { from: Uint8Array }
+type DelegationsQueryWrapperResponse = { shares: Uint8Array; to: Uint8Array }[]
+type UndelegationsQueryWrapperArgs = { to: Uint8Array }
+type UndelegationsQueryWrapperResponse = { shares: Uint8Array; from: Uint8Array; epoch: number }[]
+
 const PARATIME_RUNTIME_ID = import.meta.env.VITE_PARATIME_RUNTIME_ID
 const GRPC_URL = import.meta.env.VITE_GRPC_URL
 
 const CONSENSUS_DELEGATIONS = 'consensus.Delegations'
 const CONSENSUS_UNDELEGATIONS = 'consensus.Undelegations'
 
-type DelegationsQueryWrapperArgs = { from: Uint8Array }
-type DelegationsQueryWrapperResponse = { shares: Uint8Array; to: Uint8Array }[]
-type UndelegationsQueryWrapperArgs = { to: Uint8Array }
-type UndelegationsQueryWrapperResponse = { shares: Uint8Array; from: Uint8Array; epoch: number }[]
-
-const NodeSingleton = (() => {
-  let instance: oasis.client.NodeInternal
-
-  return {
-    getInstance: () => {
-      if (!instance) {
-        instance = new oasis.client.NodeInternal(GRPC_URL)
-      }
-      return instance
-    },
-  }
-})()
-
-const DelegationsQueryWrapperSingleton = (() => {
-  let instance: oasisRT.wrapper.QueryWrapper<DelegationsQueryWrapperArgs, DelegationsQueryWrapperResponse>
-
-  return {
-    getInstance: () => {
-      if (!instance) {
-        instance = new oasisRT.wrapper.QueryWrapper<
-          DelegationsQueryWrapperArgs,
-          DelegationsQueryWrapperResponse
-        >(oasis.misc.fromHex(PARATIME_RUNTIME_ID), CONSENSUS_DELEGATIONS)
-      }
-      return instance
-    },
-  }
-})()
-
-const UndelegationsQueryWrapperSingleton = (() => {
-  let instance: oasisRT.wrapper.QueryWrapper<UndelegationsQueryWrapperArgs, UndelegationsQueryWrapperResponse>
-
-  return {
-    getInstance: () => {
-      if (!instance) {
-        instance = new oasisRT.wrapper.QueryWrapper<
-          UndelegationsQueryWrapperArgs,
-          UndelegationsQueryWrapperResponse
-        >(oasis.misc.fromHex(PARATIME_RUNTIME_ID), CONSENSUS_UNDELEGATIONS)
-      }
-      return instance
-    },
-  }
-})()
+const nodeInternal = new oasis.client.NodeInternal(GRPC_URL)
 
 export const getDelegations = async (
   hexAddress: string,
-  node: oasis.client.NodeInternal = NodeSingleton.getInstance()
+  node: oasis.client.NodeInternal = nodeInternal
 ): Promise<Delegations> => {
-  const query = DelegationsQueryWrapperSingleton.getInstance()
-
   const from = await toSecp256k1eth(hexAddress)
-  const delegations = await query.setArgs({ from }).query(node)
+  const delegations = await new oasisRT.wrapper.QueryWrapper<
+    DelegationsQueryWrapperArgs,
+    DelegationsQueryWrapperResponse
+  >(oasis.misc.fromHex(PARATIME_RUNTIME_ID), CONSENSUS_DELEGATIONS)
+    .setArgs({ from })
+    .query(node)
 
   return delegations.map(({ to, shares }) => ({
     to: oasis.staking.addressToBech32(to),
@@ -76,12 +36,15 @@ export const getDelegations = async (
 
 export const getUndelegations = async (
   hexAddress: string,
-  node: oasis.client.NodeInternal = NodeSingleton.getInstance()
+  node: oasis.client.NodeInternal = nodeInternal
 ): Promise<Undelegations> => {
-  const query = UndelegationsQueryWrapperSingleton.getInstance()
-
   const to = await toSecp256k1eth(hexAddress)
-  const undelegations = await query.setArgs({ to }).query(node)
+  const undelegations = await new oasisRT.wrapper.QueryWrapper<
+    UndelegationsQueryWrapperArgs,
+    UndelegationsQueryWrapperResponse
+  >(oasis.misc.fromHex(PARATIME_RUNTIME_ID), CONSENSUS_UNDELEGATIONS)
+    .setArgs({ to })
+    .query(node)
 
   return undelegations.map(({ from, shares, epoch }) => ({
     from: oasis.staking.addressToBech32(from),
