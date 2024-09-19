c448690917fa04cc96d7004f2c7aba8eaf36895a
Kaja <kaja.osojnik@gmail.com>
2024-09-19 22:46:27 +0100

    Add tooltip syling

--- src/components/Hint/index.tsx
+++ src/components/Hint/index.tsx
@@ -1,8 +1,13 @@
+import classes from './index.module.css'
 export function Hint(props: { title: string; children: React.ReactNode }) {
   return (
     <div>
       {props.children}
-      {props.title}
+      <div className={classes.hintWrapper}>
+        <div className={classes.hint}>
+          {props.title}
+        </div>
+      </div>
     </div>
   )
 }
