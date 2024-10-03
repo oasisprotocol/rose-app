d174c4b281b88102347d73396e6e1a84d05436cf
lukaw3d <lukajeran@oasislabs.com>
2024-10-03 23:16:32 +0200

    Pathname shouldn't affect account derivation

--- src/deposit/siweMessageConsensusToSapphire.ts
+++ src/deposit/siweMessageConsensusToSapphire.ts
@@ -1,9 +1,3 @@
-if (import.meta.env.BASE_URL !== window.location.pathname) {
-  // Don't allow typos in pathname like https://oasisprotocol.github.io/dapp-onramp/abc or https://oasisprotocol.github.io/dapp-onramp//
-  // (it would derive a different account that is hard to reproduce)
-  throw new Error('Wrong URL')
-}
-
 /**
  * https://docs.metamask.io/wallet/how-to/sign-data/siwe/
  * https://eips.ethereum.org/EIPS/eip-4361
@@ -20,7 +14,7 @@ export function siweMessageConsensusToSapphire(address: `0x${string}`) {
     `\n` +
     `${statement}\n` +
     `\n` +
-    `URI: ${window.location.origin + window.location.pathname}\n` + // Search params shouldn't affect derivation
+    `URI: ${window.location.origin}\n` + // Search params shouldn't affect derivation
     `Version: 1\n` + // Must be 1
     `Chain ID: 23294\n` + // Sapphire Mainnet
     `Nonce: noReplayProtection\n` + // All fields must be constant to always derive the same account
