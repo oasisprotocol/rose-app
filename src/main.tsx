d6f064bf07653f6b720a9f04d2b7d36cf2e1cd36
lukaw3d <lukajeran@oasislabs.com>
2024-10-09 02:30:54 +0200

    Add sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 import { App } from './App.tsx'
 import './index.css'
+import { Sidebar } from './components/Sidebar/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Sidebar>
+      <App />
+    </Sidebar>
   </React.StrictMode>,
 )
