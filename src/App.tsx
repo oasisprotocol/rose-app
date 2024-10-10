6f465023852c3377c2fcebbaff02c9e6c5628f1c
lukaw3d <lukajeran@oasislabs.com>
2024-10-10 06:58:59 +0200

    Add links: wallet, wrose, transak

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,14 @@ import classes from './App.module.css'
 export function App() {
   return (
     <main className={classes.main}>
+      <a href="https://wallet.oasis.io/">
+        <h2>Transfer</h2>
+        <p>
+          Cras turpis quam aliquam vestibulum lectus aenean tempor diam. Ultricies cras tincidunt sit hendrerit. Varius
+          quisque pretium turpis lacus venenatis amet ac proin.
+        </p>
+        <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
+      </a>
       <a href="/stake/">
         <h2>Stake</h2>
         <p>
@@ -17,6 +25,22 @@ export function App() {
         <p>
           Dui fringilla a dui nunc diam. Et elementum tincidunt neque tristique amet dictum. Pulvinar aenean nascetur
           lobortis aenean mauris semper pretium elementum risus.
+        </p>
+        <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
+      </a>
+      <a href="https://wrose.oasis.io/">
+        <h2>Wrap</h2>
+        <p>
+          Nunc eu pharetra sed ut. Vestibulum egestas ipsum mi mauris risus aenean eget faucibus. Porttitor risus risus
+          mattis quis.
+        </p>
+        <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
+      </a>
+      <a href="https://global.transak.com/?apiKey=4b66a274-d663-42a8-a495-e1f0e88ce023&productsAvailed=BUY&cryptoCurrencyCode=ROSE&disableWalletAddressForm=false&isFeeCalculationHidden=false&exchangeScreenTitle=Purchase+ROSE+to+your+wallet&themeColor=%2318213c&defaultFiatAmount=100">
+        <h2>Buy</h2>
+        <p>
+          Aliquam sapien amet semper fermentum vel. Nec tincidunt in dolor id vitae. Ipsum mattis amet tristique
+          maecenas in in scelerisque quis.
         </p>
         <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
       </a>
