e4be01bacb40f2b87377ca0ed23df2fc5c48f83f
lukaw3d <lukajeran@oasislabs.com>
2024-10-10 06:58:18 +0200

    Add hub page at "/"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import east_svg from '@material-design-icons/svg/filled/east.svg'
+import classes from './App.module.css'
+
 export function App() {
-  return <h1>List</h1>
+  return (
+    <main className={classes.main}>
+      <a href="/stake/">
+        <h2>Stake</h2>
+        <p>
+          Sed cursus placerat quam eleifend. Ultricies eleifend tellus ut massa. Commodo elementum scelerisque iaculis
+          id.
+        </p>
+        <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
+      </a>
+      <a href="/move/">
+        <h2>Move</h2>
+        <p>
+          Dui fringilla a dui nunc diam. Et elementum tincidunt neque tristique amet dictum. Pulvinar aenean nascetur
+          lobortis aenean mauris semper pretium elementum risus.
+        </p>
+        <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
+      </a>
+    </main>
+  )
 }
