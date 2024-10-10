4535a29b6f3c72cca40c05188299f60149f7f5fa
lukaw3d <lukajeran@oasislabs.com>
2024-10-10 02:16:31 +0200

    Add sidebar to dApps

--- move/src/main.tsx
+++ move/src/main.tsx
@@ -5,6 +5,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
 
+import { Sidebar } from '../../src/components/Sidebar/index.tsx'
 import { App } from './App.tsx'
 import { config } from './wagmi.ts'
 
@@ -21,18 +22,20 @@ const rainbowkitTheme: RainbowkitTheme = {
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider
-          theme={rainbowkitTheme}
-          avatar={({ address, size }) => (
-            <AccountAvatar diameter={size} account={{ address_eth: address as `0x${string}` }} />
-          )}
-        >
-          <App />
-        </RainbowKitProvider>
-      </QueryClientProvider>
-    </WagmiProvider>
-  </React.StrictMode>,
+  <Sidebar>
+    <React.StrictMode>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <RainbowKitProvider
+            theme={rainbowkitTheme}
+            avatar={({ address, size }) => (
+              <AccountAvatar diameter={size} account={{ address_eth: address as `0x${string}` }} />
+            )}
+          >
+            <App />
+          </RainbowKitProvider>
+        </QueryClientProvider>
+      </WagmiProvider>
+    </React.StrictMode>
+  </Sidebar>,
 )
