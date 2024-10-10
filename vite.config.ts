f8c7b866f57ea9eaf8c4f33e94d597afc9cc91a8
lukaw3d <lukajeran@oasislabs.com>
2024-10-10 05:15:57 +0200

    Allow opening /move/ in dev mode, not just /move/index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'Allow opening /move/ in dev mode, not just /move/index.html',
+      configureServer(server) {
+        server.middlewares.use((req, _res, next) => {
+          if (req.url === '/move/') req.url += 'index.html'
+          if (req.url === '/stake/') req.url += 'index.html'
+          next()
+        })
+      },
+    },
+  ],
   base: '/',
 })
