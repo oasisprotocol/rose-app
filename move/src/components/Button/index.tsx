efdc593483b2dae32a623439bd54f4f29b423795
lukaw3d <lukajeran@oasislabs.com>
2024-10-17 01:28:04 +0200

    move: Remove "undefined" from button classes

--- move/src/components/Button/index.tsx
+++ move/src/components/Button/index.tsx
@@ -3,7 +3,7 @@ import classes from './index.module.css'
 
 export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button {...props} type={props.type ?? 'button'} className={`${classes.button} ${props.className}`}>
+    <button {...props} type={props.type ?? 'button'} className={`${classes.button} ${props.className ?? ''}`}>
       {props.children}
     </button>
   )
