49ac24baa919082bf4a62b648aba60a69054e552
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-10-16 06:41:22 +0200

    Drop orval support
    - due to RCE vulnerability in https://nvd.nist.gov/vuln/detail/CVE-2024-21534

--- stake/frontend/src/providers/ApiProvider.tsx
+++ stake/frontend/src/providers/ApiProvider.tsx
@@ -1,6 +1,6 @@
 import { FC, PropsWithChildren, useState } from 'react'
 import { ApiContext, ApiProviderContext, ApiProviderState } from './ApiContext'
-import { getOasisNexusAPIV1 } from '@oasisprotocol/nexus-api'
+import { Api, HttpClient } from '@oasisprotocol/nexus-api'
 import { AxiosRequestConfig } from 'axios'
 import { useWeb3 } from '../hooks/useWeb3'
 import { NEXUS_BASE_URL_CONFIG } from '../constants/config'
@@ -25,10 +25,9 @@ export const ApiContextProvider: FC<PropsWithChildren> = ({ children }) => {
     } satisfies AxiosRequestConfig
   }
 
-  const { getConsensusValidators } = getOasisNexusAPIV1()
-
   const getValidators = async (axiosRequestConfig = getAxiosRequestConfig()) => {
-    const { data } = await getConsensusValidators({ limit: 1000, offset: 0 }, axiosRequestConfig)
+    const nexusApi = new Api(new HttpClient(axiosRequestConfig))
+    const { data } = await nexusApi.consensus.validatorsList({ limit: 1000, offset: 0 })
     return data
   }
 
