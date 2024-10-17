3e8e3e4b54492f514418724f653e9b62bcaefda4
lukaw3d <lukajeran@oasislabs.com>
2024-10-17 02:33:50 +0200

    home: Open external dApps in new tab

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import classes from './App.module.css'
 export function App() {
   return (
     <main className={classes.main}>
-      <a href="https://wallet.oasis.io/" className={classes.transfer}>
+      <a href="https://wallet.oasis.io/" className={classes.transfer} target="_blank" rel="noopener noreferrer">
         <h2>Wallet</h2>
         <p>
           Cras turpis quam aliquam vestibulum lectus aenean tempor diam. Ultricies cras tincidunt sit hendrerit. Varius
@@ -29,7 +29,7 @@ export function App() {
         </p>
         <img src={east_svg} alt="Go" width="24" style={{ filter: 'invert(1)' }} />
       </a>
-      <a href="https://wrose.oasis.io/" className={classes.wrap}>
+      <a href="https://wrose.oasis.io/" className={classes.wrap} target="_blank" rel="noopener noreferrer">
         <h2>Wrap</h2>
         <p>
           Nunc eu pharetra sed ut. Vestibulum egestas ipsum mi mauris risus aenean eget faucibus. Porttitor risus risus
@@ -40,6 +40,8 @@ export function App() {
       <a
         href="https://global.transak.com/?apiKey=4b66a274-d663-42a8-a495-e1f0e88ce023&productsAvailed=BUY&cryptoCurrencyCode=ROSE&disableWalletAddressForm=false&isFeeCalculationHidden=false&exchangeScreenTitle=Purchase+ROSE+to+your+wallet&themeColor=%2318213c&defaultFiatAmount=100"
         className={classes.buy}
+        target="_blank"
+        rel="noopener noreferrer"
       >
         <h2>Buy on Transak</h2>
         <p>
