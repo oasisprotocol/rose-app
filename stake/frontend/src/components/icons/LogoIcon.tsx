e93e18dcef8070561a9976e2e823b369c95b3964
Matej Lubej <matej.lubej@oasisprotocol.org>
2024-12-09 15:11:38 +0100

    stake: Mobile sidebar
    - adjust styling to match design

--- stake/frontend/src/components/icons/LogoIcon.tsx
+++ stake/frontend/src/components/icons/LogoIcon.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react'
 import logo_rose_stake_svg from '/logo_rose_stake.svg?url'
+import { useAppState } from '../../hooks/useAppState'
 
 interface Props {
   className?: string
 }
 
 export const LogoIcon: FC<Props> = ({ className }) => {
-  return <img className={className} height="67" src={logo_rose_stake_svg} />
+  const {
+    state: { isMobileScreen },
+  } = useAppState()
+
+  return <img className={className} height={isMobileScreen ? 36 : 67} src={logo_rose_stake_svg} />
 }
--- stake/frontend/src/main.tsx
+++ stake/frontend/src/main.tsx
@@ -1,10 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
-import { Sidebar } from '../../../src/components/Sidebar'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Sidebar>
-    <App />
-  </Sidebar>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
