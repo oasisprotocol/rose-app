e6117a8c38dfd95945a525a3c397918671c4291c
Matej Lubej <matej.lubej@oasisprotocol.org>
2025-04-10 10:25:27 +0200

    wrap: Unify connect wallet page

--- wrap/src/pages/ConnectWallet/index.tsx
+++ wrap/src/pages/ConnectWallet/index.tsx
@@ -2,10 +2,13 @@ import { FC } from 'react'
 import classes from './index.module.css'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { withConnectedWallet } from '../../hoc/withConnectedWallet'
+import { WrapLogoIcon } from '@oasisprotocol/rose-app-ui'
 
 const ConnectWalletCmp: FC = () => {
   return (
-    <div>
+    <div className={classes.connectWallet}>
+      <WrapLogoIcon />
+
       <p className={classes.subHeader}>
         Quickly wrap your ROSE into wROSE and vice versa with the (un)wrap ROSE tool.
         <br />
