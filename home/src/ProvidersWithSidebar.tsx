a431cf9eba694d41ad142d5ed095ee0ae9d665c6
lukaw3d <luka@oasisprotocol.org>
2025-06-12 05:09:07 +0200

    change color of Rainbowkit ConnectButton

--- home/src/ProvidersWithSidebar.tsx
+++ home/src/ProvidersWithSidebar.tsx
@@ -10,7 +10,7 @@ import '@rainbow-me/rainbowkit/styles.css'
 
 const queryClient = new QueryClient()
 const rainbowKitTheme: Theme = {
-  ...lightTheme({ accentColor: 'var(--brand-extra-dark)' }),
+  ...lightTheme({ accentColor: 'var(--primary)', accentColorForeground: 'var(--primary-foreground)' }),
   fonts: {
     body: 'inherit',
   },
