81138720bc170bda1a2153d0d5d7c68adadad8ec
lukaw3d <luka@oasisprotocol.org>
2025-08-21 03:09:26 +0200

    Refactor subcall library using new @oasisprotocol/client methods

--- subcall/src/subcall.ts
+++ subcall/src/subcall.ts
@@ -1,6 +1,5 @@
 import { getParaTimeConfig } from './utils.ts'
-import * as cborg from 'cborg'
-import { AbiParameter, bytesToHex, encodeAbiParameters, TransactionRequest } from 'viem'
+import { TransactionRequest } from 'viem'
 import * as oasis from '@oasisprotocol/client'
 import * as oasisRT from '@oasisprotocol/client-rt'
 import { ParaTimeChainId } from './types.ts'
@@ -10,25 +9,15 @@ export const consensusDelegate = (
   to: string,
   stakeAmount: bigint
 ): TransactionRequest => {
-  const { subcallAddress } = getParaTimeConfig(paraTimeChainId)!
+  const { paratimeRuntimeId } = getParaTimeConfig(paraTimeChainId)!
 
-  const types: AbiParameter[] = [{ type: 'string' }, { type: 'bytes' }]
-  const values: [string, `0x${string}`] = [
-    'consensus.Delegate',
-    bytesToHex(
-      cborg.encode({
-        amount: [oasis.quantity.fromBigInt(stakeAmount), oasisRT.token.NATIVE_DENOMINATION],
-        to: oasis.staking.addressFromBech32(to),
-      })
-    ),
-  ]
-
-  const data = encodeAbiParameters(types, values)
-
-  return {
-    to: subcallAddress as `0x${string}`,
-    data,
-  }
+  return new oasisRT.consensusAccounts.Wrapper(oasis.misc.fromHex(paratimeRuntimeId))
+    .callDelegate()
+    .setBody({
+      amount: [oasis.quantity.fromBigInt(stakeAmount), oasisRT.token.NATIVE_DENOMINATION],
+      to: oasis.staking.addressFromBech32(to),
+    })
+    .toSubcall()
 }
 
 export const consensusUndelegate = (
@@ -36,23 +25,13 @@ export const consensusUndelegate = (
   from: string,
   shares: bigint
 ): TransactionRequest => {
-  const { subcallAddress } = getParaTimeConfig(paraTimeChainId)!
+  const { paratimeRuntimeId } = getParaTimeConfig(paraTimeChainId)!
 
-  const types: AbiParameter[] = [{ type: 'string' }, { type: 'bytes' }]
-  const values: [string, `0x${string}`] = [
-    'consensus.Undelegate',
-    bytesToHex(
-      cborg.encode({
-        shares: oasis.quantity.fromBigInt(shares),
-        from: oasis.staking.addressFromBech32(from),
-      })
-    ),
-  ]
-
-  const data = encodeAbiParameters(types, values)
-
-  return {
-    to: subcallAddress as `0x${string}`,
-    data,
-  }
+  return new oasisRT.consensusAccounts.Wrapper(oasis.misc.fromHex(paratimeRuntimeId))
+    .callUndelegate()
+    .setBody({
+      shares: oasis.quantity.fromBigInt(shares),
+      from: oasis.staking.addressFromBech32(from),
+    })
+    .toSubcall()
 }
