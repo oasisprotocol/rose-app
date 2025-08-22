2e66407060d32239db490e4c2ef049ff397f957f
Kristof Csillag <kristof@oasisprotocol.org>
2025-08-22 18:42:06 +0200

    Replace Wrap Model with Dialog from UI-Library

--- ui/src/wrap/components/index.ts
+++ ui/src/wrap/components/index.ts
@@ -1,5 +1,4 @@
 export * from './icons'
 export { Input as WrapInput } from './Input'
-export { Modal as WrapModal } from './Modal'
 export * from './Spinner'
 export { ToggleButton as WrapToggleButton } from './ToggleButton'
