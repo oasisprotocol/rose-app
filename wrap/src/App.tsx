5310fc9df959f3d7fb056bffbf96c021f119ef35
Kristof Csillag <kristof@oasisprotocol.org>
2025-09-02 15:27:50 +0200

    Wrap: eliminate local Layout component

--- wrap/src/App.tsx
+++ wrap/src/App.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react'
-import { Layout } from './components/Layout'
+import { Outlet } from 'react-router-dom'
 import { Web3ContextProvider } from './providers/Web3Provider'
 
 export const App: FC = () => (
   <Web3ContextProvider>
-    <Layout />
+    <div className={'p-6'}>
+      <Outlet />
+    </div>
   </Web3ContextProvider>
 )
