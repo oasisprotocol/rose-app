dd2774dbc8b844d9752c4410c34e030f7e6c1d61
Michal Zielenkiewicz <michal@oasisprotocol.org>
2026-01-22 08:54:45 +0100

    Update connect wall layout

--- wrap/src/pages/ConnectWallet/index.tsx
+++ wrap/src/pages/ConnectWallet/index.tsx
@@ -1,22 +1,28 @@
 import { FC } from 'react'
-import classes from './index.module.css'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { withConnectedWallet } from '../../hoc/withConnectedWallet'
-import { WrapLogoIcon } from '@oasisprotocol/rose-app-ui'
+import {
+  Card,
+  CardHeader,
+  CardDescription,
+  CardTitle,
+  CardFooter,
+} from '@oasisprotocol/ui-library/src/components/card'
+import { ConnectWalletButton } from '@oasisprotocol/rose-app-ui'
 
 const ConnectWalletCmp: FC = () => {
   return (
-    <div className={classes.connectWallet}>
-      <WrapLogoIcon />
-
-      <p className={classes.subHeader}>
-        Quickly wrap your ROSE into wROSE and vice versa with the (un)wrap ROSE tool.
-        <br />
-        Please connect your wallet to get started.
-      </p>
-
-      <ConnectButton />
-    </div>
+    <Card className="w-full max-w-[380px]">
+      <CardHeader>
+        <CardTitle>Wrap</CardTitle>
+        <CardDescription>
+          Quickly wrap your ROSE into wROSE and vice versa with the (un)wrap ROSE tool. Please connect your
+          wallet to get started.
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <ConnectWalletButton className="w-full" />
+      </CardFooter>
+    </Card>
   )
 }
 
