e3a7aa63c7b87908f321db7b19a986d596e141b5
Michal Zielenkiewicz <michal@oasisprotocol.org>
2026-01-22 12:11:00 +0100

    Update transaction status layout

--- wrap/src/pages/Transaction/index.tsx
+++ wrap/src/pages/Transaction/index.tsx
@@ -1,12 +1,14 @@
 import { FC, useEffect, useState } from 'react'
-import { WrapButton, OpenInNewIcon, Spinner } from '@oasisprotocol/rose-app-ui/wrap'
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom'
-import classes from './index.module.css'
 import { StringUtils } from '../../utils/string.utils'
 import { useWeb3 } from '../../hooks/useWeb3'
 import { WrapFormType } from '../../utils/types'
 import { TransactionBase } from 'viem'
 import { withDisconnectedWallet } from '../../hoc/withDisconnectedWallet'
+import { Button } from '@oasisprotocol/ui-library/src/components/button'
+import { Typography } from '@oasisprotocol/ui-library/src/components/typography'
+import { ExternalLink } from 'lucide-react'
+import { cn } from '@oasisprotocol/ui-library/src/lib/utils'
 
 enum TransactionStatus {
   Loading,
@@ -63,58 +65,62 @@ const TransactionCmp: FC = () => {
   }
 
   return (
-    <>
+    <div className="w-full px-8 py-12 flex flex-col items-center justify-center">
+      <div className="w-full flex items-center justify-center">
+        {/* mitigate webm black background */}
+        <video className="mix-blend-lighten" width="310" height="310" autoPlay muted loop playsInline>
+          <source src="https://assets.oasis.io/webm/Oasis-Loader-310x310.webm" type="video/webm" />
+        </video>
+      </div>
       {status === TransactionStatus.Loading && (
-        <div>
-          <div className={classes.spinnerContainer}>
-            <Spinner />
-          </div>
-          <h3 className={classes.subHeader}>
-            {action === WrapFormType.WRAP && <>Wrapping</>}
-            {action === WrapFormType.UNWRAP && <>Unwrapping</>}
-            &nbsp;your tokens
-          </h3>
-        </div>
+        <Typography variant="h2">
+          {action === WrapFormType.WRAP && <>Wrapping</>}
+          {action === WrapFormType.UNWRAP && <>Unwrapping</>}
+          &nbsp;your tokens
+        </Typography>
       )}
       {status === TransactionStatus.Success && (
-        <div>
-          <p className={classes.h100}>&#x1F389;</p>
-          <h3 className={classes.subHeader}>
-            Congrats!
-            <br />
-            You received
-            {type === TransactionType.WRose && <b>&nbsp;{amount} WROSE</b>}
-            {type === TransactionType.Rose && <b>&nbsp;{amount} ROSE</b>}
-          </h3>
-
-          {txUrl && (
-            <a className={classes.noUnderlineLink} href={txUrl} target="_blank" rel="noopener noreferrer">
-              <WrapButton className={classes.openInExplorerBtn} fullWidth>
-                View on explorer
-                <OpenInNewIcon />
-              </WrapButton>
-            </a>
-          )}
-          <WrapButton variant="secondary" onClick={handleNavigateBack} fullWidth>
-            Close
-          </WrapButton>
+        <div className="flex flex-col gap-4 text-center">
+          <div>
+            <Typography variant="h2">Token Wrap Successful</Typography>
+            <Typography className="text-muted-foreground text-center">
+              You have received {type === TransactionType.WRose && <b>{amount} WROSE</b>}
+              {type === TransactionType.Rose && <b>{amount} ROSE</b>}
+            </Typography>
+          </div>
+          <div className="flex gap-4">
+            <Button
+              variant="secondary"
+              className={cn('text-primary dark:bg-[#021617]', !txUrl && 'w-full')}
+              onClick={handleNavigateBack}
+            >
+              Back to Wrap
+            </Button>
+            {txUrl && (
+              <Button asChild variant="outline" className="text-primary dark:bg-background">
+                <a href={txUrl} target="_blank" rel="noopener noreferrer">
+                  View on explorer
+                  <ExternalLink />
+                </a>
+              </Button>
+            )}
+          </div>
         </div>
       )}
       {status === TransactionStatus.Fail && (
-        <div>
-          <p className={classes.h100}>&#x2755;</p>
-          <h3 className={classes.subHeader}>
-            There was an unexpected error.
-            <br />
-            Please try again.
-          </h3>
-
-          <WrapButton onClick={handleNavigateBack} fullWidth>
+        <div className="flex flex-col gap-4 text-center">
+          <div>
+            <Typography variant="h2">Token Wrap Failed</Typography>
+            <Typography className="text-destructive">
+              There was an unexpected error. Please try again.
+            </Typography>
+          </div>
+          <Button className="w-full text-primary dark:bg-[#021617]" onClick={handleNavigateBack}>
             Retry
-          </WrapButton>
+          </Button>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
